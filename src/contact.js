4e63de61d12ca2c638cd55ddfceed37cc0b9e39a
Mariann M <mariann19@gmail.com>
2019-06-14 12:21:06 -0400

    contactlinks

--- src/contact.js
+++ src/contact.js
@@ -6,16 +6,25 @@ const Contact = () => {
   return (
     <div className="contact-main">
       <div className="contact">
-        <a href="https://github.com/marcsi19" target="blank">
-          <img src={require("./img/github.png")} alt="github" />
+        <a
+          href="http://github.com/marcsi19"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={require("./img/github.jpg")} alt="github" />
         </a>
-        <a href="https://www.linkedin.com/in/mariannmateka/" target="blank">
+        <a
+          href="https://www.linkedin.com/in/mariannmateka/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={require("./img/linkedin.png")} alt="linkedin" />
         </a>
         {/* <a href="https://bit.ly/2ZsoiWH" target="blank"> */}
         <a
           href="https://drive.google.com/file/d/1qqR97BQZuIXYLiFQRh0U0zzHYhVxeuk9/view?usp=sharing"
-          target="blank"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <img src={require("./img/resume.png")} alt="resume" />
         </a>
