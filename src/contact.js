94cbd801ccd750b2ed2449eb010f991aeb1d90b3
Mariann M <mariann19@gmail.com>
2019-06-14 11:15:58 -0400

    contact page

--- src/contact.js
+++ src/contact.js
@@ -1,28 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import Resume from "./resume";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <div className="contact__box">
-        <Link to="https://github.com/marcsi19" target="_blank">
-          <img
-            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-            alt="github"
-          />
-        </Link>
-        <Link to="https://www.linkedin.com/in/mariannmateka/" target="_blank">
-          <img
-            src=" http://www.systemcare-ict.nl/wp-content/uploads/2015/01/linkedin-logo.png"
-            alt="linkedin"
-          />
-        </Link>
-        <Link to="https://bit.ly/2ZsoiWH" target="_blank">
-          <img
-            src="https://freepngimg.com/download/resume/3-2-resume-png.png"
-            alt="resume"
-          />
-        </Link>
+    <div className="contact-main">
+      <div className="contact">
+        <a href="https://github.com/marcsi19" target="blank">
+          <img src={require("./img/github.png")} alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/mariannmateka/" target="blank">
+          <img src={require("./img/linkedin.png")} alt="linkedin" />
+        </a>
+        {/* <a href="https://bit.ly/2ZsoiWH" target="blank"> */}
+        <a
+          href="https://drive.google.com/file/d/1qqR97BQZuIXYLiFQRh0U0zzHYhVxeuk9/view?usp=sharing"
+          target="blank"
+        >
+          <img src={require("./img/resume.png")} alt="resume" />
+        </a>
       </div>
     </div>
   );
