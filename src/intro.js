6bea7b2c41a64a410d891beb3e6c6e98823bc5ff
Mariann M <mariann19@gmail.com>
2019-06-23 14:45:28 -0700

    intro page

--- src/intro.js
+++ src/intro.js
@@ -4,10 +4,6 @@ import { Link } from "react-router-dom";
 const Intro = () => (
   <div className="main-page">
     <div className="intro">
-      <h3 className="intro__title">
-        "A​rt enables us to find ourselves and lose ourselves at the same time​"
-        <br />- Thomas Merton
-      </h3>
       <div className="intro__content">
         Hi! I'm Mariann. <br />I am a software developer.
         <br />
