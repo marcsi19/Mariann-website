900f1a241edd2cb1f100cd4ba3efcecbb61eee19
Mariann M <mariann19@gmail.com>
2019-06-23 22:09:33 -0700

    intro page

--- src/intro.js
+++ src/intro.js
@@ -5,11 +5,14 @@ const Intro = () => (
   <div className="main-page">
     <div className="intro">
       <div className="intro__content">
-        Hi! I'm Mariann. <br />I am a software developer.
-        <br />
-        <Link to="/projects" className="intro__link">
-          Have a look at my work!
-        </Link>
+        <img src={require("./img/profile.jpg")} alt="profile" />
+        <div>
+          Hi! I'm Mariann. <br />I am a software developer.
+          <br />
+          <Link to="/projects" className="intro__link">
+            Have a look at my work!
+          </Link>
+        </div>
       </div>
     </div>
   </div>
